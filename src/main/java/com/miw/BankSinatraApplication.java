84c7fe1011c2d79e0ff1a9df05e746f08a5a76c5
Elbert van Wijk <elbert.van.wijk@hva.nl>
2021-08-17 15:15:27 +0200

    kleine toevoeging (testapplicatie was nog niet included)

---
+++ src/main/java/com/miw/BankSinatraApplication.java
@@ -0,0 +1,13 @@
+package com.miw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSinatraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSinatraApplication.class, args);
+    }
+
+}
