de3e821b4825fc92cbb57f6053625c9705e887ba
Elbert van Wijk <elbert.van.wijk@hva.nl>
2021-08-17 15:52:44 +0200

    test2

--- src/main/java/com/miw/BankSinatraApplication.java
+++ src/main/java/com/miw/BankSinatraApplication.java
@@ -2,7 +2,7 @@ package com.miw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// test
 @SpringBootApplication
 public class BankSinatraApplication {
 
