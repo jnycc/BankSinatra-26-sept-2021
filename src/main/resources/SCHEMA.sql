3f5f60f65b516e320ab7d2473b5c38583f52c63d
wagenal <laura.wagenaar-buit@hva.nl>
2021-08-24 22:51:34 +0200

    nieuw create script

---
+++ src/main/resources/SCHEMA.sql
@@ -0,0 +1,134 @@
+-- MySQL Workbench Forward Engineering
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema banksinatra
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `banksinatra` ;
+
+-- -----------------------------------------------------
+-- Schema banksinatra
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `banksinatra` ;
+USE `banksinatra` ;
+
+-- -----------------------------------------------------
+-- Table `banksinatra`.`BankingFee`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `banksinatra`.`BankingFee` (
+  `percentage` DECIMAL(15,0) NOT NULL,
+  PRIMARY KEY (`percentage`))
+
+
+
+-- -----------------------------------------------------
+-- Table `banksinatra`.`Crypto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `banksinatra`.`Crypto` (
+  `cryptoID` INT NOT NULL,
+  `symbol` VARCHAR(10) NOT NULL,
+  `exchangeRate` DECIMAL(25) NOT NULL,
+  `description` VARCHAR(150) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`cryptoID`))
+
+
+-- -----------------------------------------------------
+-- Table `banksinatra`.`User`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `banksinatra`.`User` (
+  `userID` INT NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(100) NOT NULL,
+  `salt` VARCHAR(25) NOT NULL,
+  `role` VARCHAR(45) NOT NULL,
+  `isBlocked` TINYINT NOT NULL,
+  `firstName` VARCHAR(45) NOT NULL,
+  `prefix` VARCHAR(25) NOT NULL,
+  `lastName` VARCHAR(100) NOT NULL,
+  `street` VARCHAR(100) NULL,
+  `houseNumber` INT NULL,
+  `houseNumberExtension` VARCHAR(25) NULL,
+  `zipCode` VARCHAR(25) NULL,
+  `city` VARCHAR(45) NULL,
+  `bsn` INT NULL,
+  `dateOfBirth` DATE NULL,
+  PRIMARY KEY (`userID`))
+
+
+-- -----------------------------------------------------
+-- Table `banksinatra`.`Account`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `banksinatra`.`Account` (
+  `accountID` INT NOT NULL,
+  `IBAN` VARCHAR(45) NOT NULL,
+  `balance` DECIMAL(10,0) NOT NULL,
+  `User_userID` INT NOT NULL,
+  PRIMARY KEY (`accountID`),
+  INDEX `fk_Account_User1_idx` (`User_userID` ASC) VISIBLE,
+  CONSTRAINT `fk_Account_User1`
+    FOREIGN KEY (`User_userID`)
+    REFERENCES `banksinatra`.`User` (`userID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+
+
+-- -----------------------------------------------------
+-- Table `banksinatra`.`Transaction`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `banksinatra`.`Transaction` (
+  `transactionID` INT NOT NULL,
+  `date` DATETIME NOT NULL,
+  `units` DECIMAL(25,0) NOT NULL,
+  `exchangeRate` DECIMAL(25) NOT NULL,
+  `bankingFee` DECIMAL(15,0) NOT NULL,
+  `Account_accountID_buyer` INT NOT NULL,
+  `Account_accountID_seller` INT NOT NULL,
+  `Crypto_cryptoID` INT NOT NULL,
+  PRIMARY KEY (`transactionID`),
+  INDEX `fk_Transaction_Account1_idx` (`Account_accountID_buyer` ASC) VISIBLE,
+  INDEX `fk_Transaction_Account2_idx` (`Account_accountID_seller` ASC) VISIBLE,
+  INDEX `fk_Transaction_Crypto1_idx` (`Crypto_cryptoID` ASC) VISIBLE,
+  CONSTRAINT `fk_Transaction_Account1`
+    FOREIGN KEY (`Account_accountID_buyer`)
+    REFERENCES `banksinatra`.`Account` (`accountID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Transaction_Account2`
+    FOREIGN KEY (`Account_accountID_seller`)
+    REFERENCES `banksinatra`.`Account` (`accountID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Transaction_Crypto1`
+    FOREIGN KEY (`Crypto_cryptoID`)
+    REFERENCES `banksinatra`.`Crypto` (`cryptoID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+
+
+-- -----------------------------------------------------
+-- Table `banksinatra`.`Asset`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `banksinatra`.`Asset` (
+  `Account_accountID` INT NOT NULL,
+  `Crypto_cryptoID` INT NOT NULL,
+  `units` DECIMAL(10) NOT NULL,
+  PRIMARY KEY (`Account_accountID`, `Crypto_cryptoID`),
+  INDEX `fk_Account_has_Crypto_Crypto1_idx` (`Crypto_cryptoID` ASC) VISIBLE,
+  INDEX `fk_Account_has_Crypto_Account1_idx` (`Account_accountID` ASC) VISIBLE,
+  CONSTRAINT `fk_Account_has_Crypto_Account1`
+    FOREIGN KEY (`Account_accountID`)
+    REFERENCES `banksinatra`.`Account` (`accountID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Account_has_Crypto_Crypto1`
+    FOREIGN KEY (`Crypto_cryptoID`)
+    REFERENCES `banksinatra`.`Crypto` (`cryptoID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+
+-- Gebruiker definiëren en toegang verlenen
+CREATE USER 'cursist'@'localhost' IDENTIFIED BY 'cohort';
+GRANT ALL PRIVILEGES ON `banksinatra` . * TO 'cursist'@'localhost';
