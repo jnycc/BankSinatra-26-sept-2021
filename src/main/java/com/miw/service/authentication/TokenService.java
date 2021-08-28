187aa5162276a567b96f0e5718d1ff123dbaa9cb
Maran Leen <maran.leen@hva.nl>
2021-08-28 15:48:37 +0200

    jwt aangepast

--- src/main/java/com/miw/service/authentication/TokenService.java
+++ src/main/java/com/miw/service/authentication/TokenService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.crypto.spec.SecretKeySpec;
+import javax.management.relation.Role;
 import javax.xml.bind.DatatypeConverter;
 import java.security.Key;
 import java.util.Date;
@@ -17,6 +18,7 @@ import java.util.UUID;
 @Service
 public class TokenService {
 
+    private static String jwt;
     private JdbcTokenDao jdbcTokenDao;
 
     @Autowired
@@ -27,37 +29,35 @@ public class TokenService {
     public TokenService() {
     }
 
-    public String generateToken() {
-        return UUID.randomUUID().toString();       // TODO: omzetten in jason Web token?
-    }
+    // Generating and validating jwt
 
+    public String generateJwt(String id, String userEmail, long ttlMillis){
 
-
-    public String generateJwt(String id, String issuer, String userEmail) {//, long ttlMillis){
-
-        //The JWT signature algorithm we will be using to sign the token
+        //JWT signature algorithm:
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
         long nowMillis = System.currentTimeMillis();
         Date dateIssued = new Date(nowMillis);
 
         //We will sign our JWT with our ApiKey secret
-        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary("banaan");
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary("pepper");
         Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
 
-        //Let's set the JWT Claims
-        JwtBuilder builder = Jwts.builder().setId(id)
+        //set JWT Claims
+        JwtBuilder builder = Jwts.builder()
+                .setId(id)
                 .setIssuedAt(dateIssued)
                 .setSubject(userEmail)
-                .setIssuer(issuer)
+                //TODO: voeg rol toe in payload.
+                //.setClaims("roles", jdbcUserDao.getRoleByEmail(userEmail))
                 .signWith(signatureAlgorithm, signingKey);
 
-        //if it has been specified, let's add the expiration
-/*        if (ttlMillis > 0) {
+        //add expiration date and time
+        if (ttlMillis > 0) {
             long expMillis = nowMillis + ttlMillis;
             Date exp = new Date(expMillis);
            builder.setExpiration(exp);
-        }*/
+        }
 
         //Builds the JWT and serializes it to a compact, URL-safe string
         return builder.compact();
@@ -65,21 +65,23 @@ public class TokenService {
 
     public static Claims decodeJWT(String jwt) {
         //This line will throw an exception if it is not a signed JWS (as expected)
-        Claims claims = Jwts.parser()
-                .setSigningKey(DatatypeConverter.parseBase64Binary("HIER KOMT DE PEPPER?"))
+        return Jwts.parser()
+                .setSigningKey(DatatypeConverter.parseBase64Binary("pepper"))
                 .parseClaimsJws(jwt).getBody();
-        return claims;
+    }
+
+
+
+    // Generating and validating UUID token:
+
+    public String generateToken() {
+        return UUID.randomUUID().toString();       // TODO: omzetten in jason Web token?
     }
 
     public boolean validateToken(String token) {
         // TODO: checken op datum?
         return jdbcTokenDao.retrieveToken(token) != null;
     }
-
-/*    public void expireToken(String token) {
-        deleteToken(token);
-    }*/
-
 
     // TODO: methode creeren die token meegeeft via de header --> IN HTML
 
@@ -107,7 +109,7 @@ public class TokenService {
  * en is de signature waaraan de site kan zien dat de JWT legitiem is (crytocraphic hash)
  *
  * Header and payload zijn omgezet in een string aan de hand van Based64 encoded
- * (niet als beveiliging maar voor de convienence) -> voor de server to validate if info is corect.
+ * (niet als beveiliging maar voor de convienence) -> voor de server to validate if info is correct.
  * Verify signature: created by server with help of the secret (pepper)
  * is gelinkt aan de header en payload
  *
@@ -115,7 +117,7 @@ public class TokenService {
  * - user logt in
  * - gegevens worden gecheckt (authenticatie)
  * - jwt wordt gecreeert
- * - jwt wordt terug gestuurd naar de user (local storage of coocie)
+ * - jwt wordt terug gestuurd naar de user (local storage of cookie)
  * - With every new user request:
  *       - jwt gaat terug naar de server
  *       - via HTTP HEADER: key/value names: key: Authoraziation, value: Bearer JWT
@@ -123,30 +125,10 @@ public class TokenService {
  *           1. splitsen in drie delen
  *           2. based64 decoding of part 1 en 2
  *           3. calculates signature of this decoding
- *           4. checkes if this calculated signature matches the given signature (part 3)
+ *           4. checks if this calculated signature matches the given signature (part 3)
  *
  * Expiration payload: (little tricky)
  * - Blacklisted JWT --> JWT's that are no longer valid
  * */
 
 // header en payload omzetten in json
-
-// header en payload coderen met code64
-//headerEncoded = Base64.getEncoder().encode(header);
-//headerEncoded = Base64.getEncoder().encode(payload);
-
-
-// header + payload + pepper coderen met code64 -> signature
-// gecodeerde header + . + payload + . + signature opslaan en returnen
-
-
-//jwt = "header" + "." + "payload" + "." + "signature";
-
-
-//if it has been specified, let's add the expiration
-
-
-// bij authenticatie wordt token aangemaakt en opgeslagen in aparte tabel in de database
-// bij het doorgaan naar een nieuwe pagina wordt token vanuit de gerbuiker kant doorgegeven (via HTML)
-// doorgegeven token wordt afgezet tegenover token in database ter validatie
-
--- src/test/java/com/miw/service/authentication/TokenServiceTest.java
+++ src/test/java/com/miw/service/authentication/TokenServiceTest.java
@@ -1,30 +1,50 @@
 package com.miw.service.authentication;
 
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.impl.DefaultClaims;
 import org.junit.jupiter.api.Test;
 
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
-import java.security.Key;
-import java.util.Arrays;
+import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class TokenServiceTest {
 
+    ByteArrayToHexHelper byteArrayToHexHelper = new ByteArrayToHexHelper();
+    TokenService tokenService = new TokenService();
+
     @Test
     void generateJwt() {
-        TokenService tokenService = new TokenService();
-        String jwt = tokenService.generateJwt("123","issuer", "subject");
-        System.out.println(jwt);
+        String actualJwt = tokenService.generateJwt("123", "test@testen.nl", 100000); //10 min
+        System.out.println(actualJwt);
 
-//        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
-//        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary("banaan");
-//        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
-//        System.out.println(Arrays.toString(signingKey.getEncoded()));
+        Claims actualClaim = tokenService.decodeJWT("eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxMjMiLCJpYXQiOjE2MzAxNTgyODYsInN1YiI6InRlc3RAdGVzdGVuLm5sIiwiZXhwIjoxNjMwMTU4Mzg2fQ.mycF1TgysfbYdGyu9ayBmBnA91PtW_7zBylkP95qN2s");
+        System.out.println(actualClaim);
+
+
+        //TODO: expected jwt string toevoegen die rekening houdt met meegegeven tijd
+        String expectedJwt = "";
+        //assertEquals(actualJwt, expectedJwt);
     }
 
 
+    @Test
+    public void decodeJWT(String jwt) {
+        //TODO: fixen van waarom onderstaande niet werkt..
+//       Claims expectedClaim = new ;
+//        expectedClaim.put("jti", 123);
+//        expectedClaim.put("iat", 1630153047);
+//        expectedClaim.put("subject", "subject");
+//        expectedClaim.put("iss", "issuer");
+//        expectedClaim.put("exp", 1630153647);
+//       System.out.println(expectedClaim);
+
+
+//        Claims actualClaim = tokenService.decodeJWT("eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxMjMiLCJpYXQiOjE2MzAxNTQ4NzMsInN1YiI6InN1YmplY3QiLCJpc3MiOiJpc3N1ZXIifQ.G186f5H_aLbRFGPpAUmrNe9vWO2cWIToVfVq29dX7NY");
+//        System.out.println(actualClaim);
+//        assertEquals(actualClaim, expectedClaim);
+
+
+    }
 }
