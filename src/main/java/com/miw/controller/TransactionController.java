1945ae08697d91b3629c1a27d24fa87d90c4a18d
Renée Jansen <reneejansen93@gmail.com>
2021-09-02 13:59:18 +0200

    Eerste uitwerking Dao, Service en Controller voor Transaction

--- src/main/java/com/miw/controller/TransactionController.java
+++ src/main/java/com/miw/controller/TransactionController.java
@@ -1,2 +1,47 @@
-package com.miw.controller;public class TransactionController {
+package com.miw.controller;
+
+import com.miw.model.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TransactionController {
+
+    private final Logger logger = LoggerFactory.getLogger(TransactionController.class);
+
+    @Autowired
+    public TransactionController(){
+        super();
+        logger.info("New TransactionController-object created");
+    }
+
+   /* @PostMapping("/buy")
+    public ResponseEntity<?> doTransaction(@RequestBody String something){
+
+    }*/
+
+    /*
+    * TODO: methodes om Transactie te kunnen voltooien
+    *
+    * Transactie =
+    * 1. Ingelogde Client klikt op Crypto die hij wil kopen & geeft aan hoeveel ervan hij wilt hebben (Post request?)
+    * 2. Checks:
+    *       a) Heeft seller het aantal crypto dat buyer wil kopen (e.g. buyer kan niet 5 BC kopen van seller als seller maar 2 BC heeft)?
+    *       b) Heeft buyer genoeg saldo op zijn rekening om de transactie te voltooien? (= prijs van crypto + bankkosten!)
+    *       c) Indien buyer & seller Clients zijn: heeft seller genoeg geld op zijn rekening om 50% van de bankkosten te betalen?
+    * 3. Het aantal crypto wordt uit het portfolio van seller gehaald en in het portfolio van buyer gestopt
+    * 4. De prijs van de crypto wordt uit het Account van buyer gehaald en in het Account van seller gestopt
+    * 5. Bankkosten verrekenen:
+    *       a) Buyer & seller zijn allebei Client --> betalen elk 50% van de bankkosten. Wordt uit hun Account gehaald en in het Account van bank gestopt
+    *       b) Buyer is Client en seller is Bank --> buyer betaalt 100% van de bankkosten. Wordt uit zijn Account gehaald en in het Account van bank gestopt
+    *       c) Buyer is Bank en seller is Client --> seller betaalt 100% van de bankkosten. Wordt uit zijn Account gehaald en in het Account van bank gestopt
+    * 6. Transactie afgerond --> transactie wordt opgeslagen in de database
+    * */
+
+
 }
--- src/main/java/com/miw/database/JdbcTransactionDao.java
+++ src/main/java/com/miw/database/JdbcTransactionDao.java
@@ -1,2 +1,51 @@
-package com.miw.database;public class JdbcTransactionDao {
+package com.miw.database;
+
+import com.miw.model.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+
+@Repository
+public class JdbcTransactionDao {
+
+    private final Logger logger = LoggerFactory.getLogger(JdbcTransactionDao.class);
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JdbcTransactionDao(JdbcTemplate jdbcTemplate) {
+        super();
+        this.jdbcTemplate = jdbcTemplate;
+        logger.info("New JdbcTransactionDao");
+    }
+
+    private PreparedStatement insertTransactionStatement(Transaction transaction, Connection connection) throws SQLException{
+        PreparedStatement ps = connection.prepareStatement("INSERT INTO Transaction " +
+                "(date, units, exchangeRate, bankingFee, accountID_buyer, accountID_seller, cryptoID) " +
+                "VALUES(?, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+        ps.setDate(1, java.sql.Date.valueOf(transaction.getTransactionDate().toLocalDate())); //TODO: dit moet anders kunnen denk ik :)
+        ps.setDouble(2, transaction.getUnits());
+        ps.setDouble(3, transaction.getCrypto().getExchangeRate());
+        ps.setDouble(4, transaction.getBankCosts());
+        ps.setInt(5, transaction.getBuyer().getUserId());
+        ps.setInt(6, transaction.getSeller().getUserId());
+        ps.setInt(7, transaction.getCrypto().getCryptoId()); //TODO: manier om CryptoID te setten!
+        return ps;
+    }
+
+    public Transaction save(Transaction transaction){
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> insertTransactionStatement(transaction, connection), keyHolder);
+        int transactionId = keyHolder.getKey().intValue();
+        transaction.setTransactionId(transactionId);
+        logger.info("New transaction has been saved to the database");
+        return transaction;
+    }
+
 }
