06f0b0102b8df33be7d22a9741feb0799e4d5720
Nijad Nazarli <nijad.nazarli@hva.nl>
2021-09-07 20:36:45 +0200

    Front End Login en Register werkend met input validaties, Admin func ook aanwezig

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,59 +1,48 @@
 // Loginsectie
-const btnLogin = document.querySelector("#btnLogin")
 const btnRegister = document.querySelector("#btnRegister")
-const btnSubmitReg = document.querySelector("#submitReg")
 const overlay = document.querySelector("#overlay")
+const loginForm = document.querySelector("#loginForm")
+const registerForm = document.querySelector("#registerFields")
+const closeOverlayBtn = document.querySelector("#close-overlay-btn")
+const adminBtn = document.querySelector("#admin")
+const clientBtn = document.querySelector("#client")
 
-btnLogin.addEventListener("click", doLogin);
+// TODO pas linkjes aan naar remote
+const userLogin = 'http://localhost:8080/login';
+const adminLogin = 'http://localhost:8080/admin/login';
+const userRegister = 'http://localhost:8080/register';
+const adminRegister = 'http://localhost:8080/admin/register';
+let currentLogin = 'http://localhost:8080/login';
+let currentRegister = 'http://localhost:8080/register';
 
-/*btnRegister.addEventListener("click", () => {
-    console.log("clicked register")
-    let email = document.querySelector("#email").value;
-    fetch(`http://localhost:8080/gegevens/${email}`, {
-        method: 'GET',
-        headers: {'Authorization':`${localStorage.getItem('token')}`},
-    })
-        .then(res => res.json())
-        .then(it => {
-            document.querySelector("h1").innerHTML = `Welcome, ${it.firstName}`
-        })
-        .catch(console.log('oeps'))
-
-})*/
-
-// Overlay example
 btnRegister.addEventListener('click', toggleOverlay);
-// overlay.addEventListener('click', toggleOverlay);
+closeOverlayBtn.addEventListener('click', closeOverlay);
+adminBtn.addEventListener('click', switchToAdmin);
+clientBtn.addEventListener('click', switchToClient);
 
-// Attempt Registering
-btnSubmitReg.addEventListener('click', doRegister);
-
-/*function createRegisterBox() {
-    document.querySelector("#overlay").style.display = "block";
-
-    const div = document.createElement("div");
-    div.id= "registerFields";
-    const divh1 = document.createElement("h1");
-    divh1.innerHTML = "Register";
-    div.appendChild(divh1);
-
-    if (document.querySelector("#overlay").querySelector("#registerFields")) {
-        console.log("register fields already exists")
-    } else {
-        document.body.querySelector('#overlay').appendChild(div);
+loginForm.addEventListener('submit', function(e) {
+    if(loginForm.children.namedItem("email").validity.valid && loginForm.children.namedItem("password").validity.valid) {
+        e.preventDefault();
+        doLogin(currentLogin);
     }
+});
 
-    document.querySelector("#overlay").addEventListener('click', toggleOverlay);
-}*/
-
-
+registerForm.addEventListener('submit', function (e) {
+    if (checkRegistrationFields()) {
+        e.preventDefault();
+        doRegister(currentRegister);
+    }
+})
 
 function toggleOverlay() {
-    overlay.style.display = overlay.style.display === 'none' ? 'block' : 'none';
+    overlay.style.display = 'block';
+}
+// TODO invulvelden leeg maken ?
+function closeOverlay() {
+    overlay.style.display = 'none';
 }
 
-
-// Show welcome screen example
+// TODO haal deze uit
 function showWelcomeScreen() {
     document.body.removeChild(document.querySelector("#loginForm"));
     document.body.removeChild(document.querySelector("#slogan"));
@@ -61,14 +50,12 @@ function showWelcomeScreen() {
     const h1 = document.createElement("h1");
     h1.id = "new-header";
     h1.value = "new header";
-    // h1.setAttribute("id", "new-header")
-    // h1.setAttribute("value", "new header")
     h1.innerHTML = "Welcome to Bank Sinatra"
 
     document.body.appendChild(h1);
 }
 
-function doRegister() {
+function doRegister(currentRegister) {
     let payload =
         {email: `${document.querySelector("#email-reg").value}`,
             password: `${ document.querySelector("#password-reg").value}`,
@@ -76,7 +63,7 @@ function doRegister() {
             prefix: `${document.querySelector("#prefix").value}`,
             lastName: `${document.querySelector("#lastName").value}`,
             dateOfBirth: `${document.querySelector("#dob").value}`,
-            bsn: `${document.querySelector("#bsn").value}`,
+            bsn: parseInt(`${document.querySelector("#bsn").value}`),
             address: {
                 city: `${document.querySelector("#city").value}`,
                 zipCode: `${document.querySelector("#zipcode").value}`,
@@ -87,9 +74,8 @@ function doRegister() {
         }
 
     let jsonString = JSON.stringify(payload);
-    console.log(jsonString)
 
-    fetch('http://localhost:8080/register',
+    fetch(`${currentRegister}`,
         {
             method: 'POST',
             header: { "Content-Type": "application/json" },
@@ -99,8 +85,12 @@ function doRegister() {
             if (res.status === 201) {
                 console.log(res.text());
                 alert("Thank you. Your requested has been received. We will process it accordingly and come back to you as soon as possible.")
+            } else if (res.status === 409) {
+                alert("Registration failed. User with this email address already exists")
+                return;
             } else {
                 alert("Registration failed");
+                return;
             }
             return res.text();
         })
@@ -110,20 +100,14 @@ function doRegister() {
         .catch()
 }
 
-function doLogin(){
+function doLogin(currentLogin){
     let payload =
         {email: `${document.querySelector("#email").value}`,
         password: `${ document.querySelector("#password").value}`}
 
-    if(payload.email === "" || payload.password === "") {
-        alert("Please enter full credentials")
-        return;
-    }
-
     let jsonString = JSON.stringify(payload);
-    console.log(jsonString)
 
-    fetch('http://localhost:8080/login',
+    fetch(`${currentLogin}`,
         {
             method: 'POST',
             header: { "Content-Type": "application/json" },
@@ -145,5 +129,43 @@ function doLogin(){
             localStorage.setItem('token', it)
         })
         .catch()
-
 }
+
+function switchToAdmin(){
+    currentLogin = adminLogin;
+    currentRegister = adminRegister;
+}
+
+function switchToClient(){
+    currentLogin = userLogin;
+    currentRegister = userRegister;
+}
+
+function checkRegistrationFields() {
+    return registerForm.children.namedItem("email-reg").validity.valid
+    && registerForm.children.namedItem("password-reg").validity.valid
+    && registerForm.children.namedItem("firstName-reg").validity.valid
+    && registerForm.children.namedItem("prefix-reg").validity.valid
+    && registerForm.children.namedItem("lastName-reg").validity.valid
+    && registerForm.children.namedItem("bsn-reg").validity.valid
+    && registerForm.children.namedItem("city-reg").validity.valid
+    && registerForm.children.namedItem("zipcode-reg").validity.valid
+    && registerForm.children.namedItem("street-reg").validity.valid
+    && registerForm.children.namedItem("housenumber-reg").validity.valid
+    && registerForm.children.namedItem("extension-reg").validity.valid
+}
+
+/*btnRegister.addEventListener("click", () => {
+    console.log("clicked register")
+    let email = document.querySelector("#email").value;
+    fetch(`http://localhost:8080/gegevens/${email}`, {
+        method: 'GET',
+        headers: {'Authorization':`${localStorage.getItem('token')}`},
+    })
+        .then(res => res.json())
+        .then(it => {
+            document.querySelector("h1").innerHTML = `Welcome, ${it.firstName}`
+        })
+        .catch(console.log('oeps'))
+
+})*/
