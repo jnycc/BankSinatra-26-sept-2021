ea6371b3c6cabea4d28bbcc9c19ab96674336e77
Johnny Chan <johnny.chan@hva.nl>
2021-09-08 17:25:10 +0200

    JWT-validatie toegevoegd aan portfolio endpoint

--- src/main/java/com/miw/controller/PortfolioController.java
+++ src/main/java/com/miw/controller/PortfolioController.java
@@ -1,8 +1,11 @@
+/**
+ * @Author: Johnny Chan
+ * @Description: Controller which validates the received JWT and obtains and returns the client's portfolio
+ * with current, historical and delta values.
+ */
 package com.miw.controller;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.miw.model.Client;
 import com.miw.service.PortfolioService;
 import com.miw.service.authentication.TokenService;
 import org.slf4j.Logger;
@@ -11,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,19 +33,12 @@ public class PortfolioController {
     }
 
     @GetMapping("/portfolio")
-    public ResponseEntity<?> getPortfolioOverview(@RequestHeader("Authorization") String token
-                                                  //@RequestBody int userId
-                                                  ) {
+    public ResponseEntity<?> getPortfolioOverview(@RequestHeader("Authorization") String token) {
         System.out.println("Test, token vooraf is: " + token);
-        int userId = TokenService.getUserID(token);
+        int userId = TokenService.getValidUserID(token);
         if (userId == 0) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
-//        JsonObject jsonObject = gson.fromJson(emailAsJson, JsonObject.class); //verander de json in een json-object
-//        String email = jsonObject.get("email").getAsString(); //haal o.b.v. key de value uit json-object
-        //System.out.printf("Json:\n%s\nConverted string: %s\n", emailAsJson, email);
-        //Eerst userId vinden van de client
-//        Client client = portfolioService.findClientByEmail(email);
         //PortfolioService aanroepen om de vereiste gegevens te verzamelen en returnen aan frontend
         Map<String, Object> portfolio = portfolioService.getPortfolio(userId);
         return ResponseEntity.ok(portfolio);
