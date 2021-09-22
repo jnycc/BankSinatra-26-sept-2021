910a3d5525c51eb04a474941a47d22877721116f
Johnny Chan <johnny.chan@hva.nl>
2021-09-22 15:42:26 +0200

    stats graph in overlay van portfolio gezet

--- src/main/resources/static/portfolio.js
+++ src/main/resources/static/portfolio.js
@@ -42,6 +42,7 @@ window.onclick = function (event) {
     }
 }
 const cryptoName = document.getElementById('cryptoName')
+let dataMap = null //globale placeholder voor de statistical data
 
 //Load total portfolio values
 window.addEventListener("DOMContentLoaded", () => {
@@ -130,9 +131,98 @@ function getCryptoLogo(symbol) {
 function openDetails(symbol, name, units) {
     $(cryptoName).text(name + " (" + symbol + ")")
     $(cryptoOverlay).show()
+    createGraph(symbol, 7)
     // TODO toon crypto grafiek in contentFeature div
     cryptoChosen = symbol;
     availableUnits = units;
+}
+
+async function createGraph(symbol, daysBack) {
+    await getAssetStats(symbol, daysBack)
+    var dataPoints1 = [], dataPoints2 = [];
+    var stockChart = new CanvasJS.StockChart("stockChartContainer", {
+        animationEnabled: true,
+        theme: "light2",
+        title: {
+            text: `${symbol} price chart`, //TODO: dit vullen met cryptonaam ipv symbol
+            fontFamily: "Calibri,Optima,Arial,sans-serif"
+        },
+        charts: [{
+            toolTip: {
+                shared: true
+            },
+            axisX: {
+                valueFormatString: "D MMM YYYY"
+            },
+            axisY: {
+                prefix: "USD "
+            },
+            data: [{
+                name: "Min-Max",
+                type: "rangeArea",
+                xValueFormatString: "DD-MM-YYY",
+                yValueFormatString: "$#,###.##",
+                dataPoints: dataPoints1
+            }, {
+                name: "Average",
+                type: "line",
+                yValueFormatString: "$#,###.##",
+                dataPoints: dataPoints2
+            }]
+        }],
+        navigator: {
+            data: [{
+                dataPoints: dataPoints2
+            }],
+            axisX: {
+                labelFontColor: "transparent",
+                labelFontWeight: "bolder",
+            },
+            slider: {
+                minimum: new Date(2020, 0o0),
+                maximum: new Date(2021, 12)
+            }
+        },
+        rangeSelector: {
+            buttons: [{
+                label: "1D",
+                range: 1,
+                rangeType: "day"
+            }, {
+                label: "7D",
+                range: 7,
+                rangeType: "day"
+            }, {
+                label: "1Y",
+                range: 1,
+                rangeType: "year"
+            }, {
+                label: "All",
+                range: null,
+                rangeType: "all"
+            }]
+        }
+    });
+    console.log("Pushmethode runt. 2. Data bestaat uit:")
+    console.log(dataMap)
+    for (let date of Object.keys(dataMap)) {
+        dataPoints1.push({x: new Date(date), y:[Number(dataMap[date].min), Number(dataMap[date].max)]});
+        dataPoints2.push({x: new Date(date), y:Number(dataMap[date].avg)});
+    }
+    stockChart.render();
+}
+
+async function getAssetStats(symbol, daysBack) {
+    await fetch(`/cryptoStats?symbol=${symbol}&daysBack=${daysBack}`, {
+        method: 'GET',
+        headers: {'Authorization': localStorage.getItem('token')}
+    })
+        .then(res => res.json())
+        .then(json => {
+            dataMap = json
+            console.log("1. Data opgehaald: ")
+            console.log(dataMap)
+        })
 }
 
 function setUpMarketAsset() {
--- src/main/resources/static/statistics.js
+++ src/main/resources/static/statistics.js
@@ -1,121 +1,97 @@
-//JSON-object met daarin een map in een map. "datum: klaarzetten
-let dataMap = null
-// window.onload = getAssetStats('BTC', 7)
-//TODO: eventlistener veranderen in: als overlay opent (functie openDetails) -> creëer graph
-window.addEventListener("DOMContentLoaded", async () => {
-    // await getAssetStats('BTC', 7)
-    await getAssetStats('BTC', 7)
-    createGraph('BTC')
-})
-// window.onload = async () => {
-//     await getAssetStats('BTC', 7)
-//     await createGraph()
+// //JSON-object met daarin een map in een map: "datum: {avg: value, min: value, max: value}" klaarzetten
+// let dataMap = null
+// // window.onload = getAssetStats('BTC', 7)
+// //TODO: eventlistener veranderen in: als overlay opent (functie openDetails) -> creëer graph
+//
+// window.addEventListener("DOMContentLoaded", async () => {
+//     // await getAssetStats('BTC', 365)
+//     createGraph('BTC', 300)
+// })
+//
+// async function getAssetStats(symbol, daysBack) {
+//     await fetch(`/cryptoStats?symbol=${symbol}&daysBack=${daysBack}`, {//vullen met endpoint
+//         method: 'GET',
+//         headers: {'Authorization': localStorage.getItem('token')}
+//     })
+//         .then(res => res.json())
+//         .then(json => {
+//             dataMap = json
+//             console.log("1. Data opgehaald: ")
+//             console.log(dataMap)
+//         })
 // }
-
-// async function setupStats() {
-//     await getAssetStats('BTC', 7)
-//     createGraph()
-// }
-
-async function getAssetStats(symbol, daysBack) {
-    await fetch(`/cryptoStats?symbol=${symbol}&daysBack=${daysBack}`, {//vullen met endpoint
-        method: 'GET',
-        headers: {'Authorization': localStorage.getItem('token')}
-    })
-        .then(res => res.json())
-        .then(json => {
-            dataMap = json
-            console.log("1. Data opgehaald: ")
-            console.log(dataMap)
-            // createGraph(symbol)//TODO: dit apart zetten/aanroepen
-        })
-}
-
-
-function createGraph(symbol) {
-    var dataPoints1 = [], dataPoints2 = [];
-    var stockChart = new CanvasJS.StockChart("stockChartContainer", {
-        animationEnabled: true,
-        theme: "light2",
-        title: {
-            text: `${symbol} price chart`, //TODO: dit vullen met cryptonaam ipv symbol
-            fontFamily: "Calibri,Optima,Arial,sans-serif"
-        },
-        // subtitles: [{
-        //     text: "Crypto price chart",//TODO: deze mogelijk weghalen
-        //     fontFamily: "Calibri,Optima,Arial,sans-serif"
-        // }],
-        charts: [{
-            toolTip: {
-                shared: true
-            },
-            axisX: {
-                valueFormatString: "D MMM YYYY"
-            },
-            axisY: {
-                prefix: "USD "
-            },
-            data: [{
-                name: "Min-Max",
-                type: "rangeArea",
-                xValueFormatString: "DD-MM-YYY",
-                yValueFormatString: "$#,###.##",
-                dataPoints: dataPoints1
-            }, {
-                name: "Average",
-                type: "line",
-                yValueFormatString: "$#,###.##",
-                dataPoints: dataPoints2
-            }]
-        }],
-        navigator: {
-            data: [{
-                dataPoints: dataPoints2
-            }],
-            axisX: {
-                labelFontColor: "transparent",
-                labelFontWeight: "bolder",
-            },
-            slider: {
-                minimum: new Date(2020, 0o0),
-                maximum: new Date(2021, 12)
-            }
-        },
-        rangeSelector: {
-            buttons: [{
-                label: "1D",
-                range: 1,
-                rangeType: "day"
-            }, {
-                label: "7D",
-                range: 7,
-                rangeType: "day"
-            }, {
-                label: "1Y",
-                range: 1,
-                rangeType: "year"
-            }, {
-                label: "All",
-                range: null,
-                rangeType: "all"
-            }]
-        }
-    });
-    console.log("Pushmethode runt. 2. Data bestaat uit:")
-    console.log(dataMap)
-    for (let date of Object.keys(dataMap)) {
-        dataPoints1.push({x: new Date(date), y:[Number(dataMap[date].min), Number(dataMap[date].max)]});
-        dataPoints2.push({x: new Date(date), y:Number(dataMap[date].avg)});
-    }
-    stockChart.render();
-
-// $.getJSON("https://canvasjs.com/data/gallery/stock-chart/weather-india.json", function (dummyData) {
-//     console.log("pushmethode runt")
-//     for (var i = 0; i < dummyData.length; i++) {
-//         dataPoints1.push({x: new Date(dummyData[i].year, 0o0, 0o0), y: [Number(dummyData[i].min), Number(dummyData[i].max)]});
-//         dataPoints2.push({x: new Date(dummyData[i].year, 0o0, 0o0), y: Number(dummyData[i].avg)});
+//
+// async function createGraph(symbol, daysBack) {
+//     await getAssetStats(symbol, daysBack)
+//     var dataPoints1 = [], dataPoints2 = [];
+//     var stockChart = new CanvasJS.StockChart("stockChartContainer", {
+//         animationEnabled: true,
+//         theme: "light2",
+//         title: {
+//             text: `${symbol} price chart`, //TODO: dit vullen met cryptonaam ipv symbol
+//             fontFamily: "Calibri,Optima,Arial,sans-serif"
+//         },
+//         charts: [{
+//             toolTip: {
+//                 shared: true
+//             },
+//             axisX: {
+//                 valueFormatString: "D MMM YYYY"
+//             },
+//             axisY: {
+//                 prefix: "USD "
+//             },
+//             data: [{
+//                 name: "Min-Max",
+//                 type: "rangeArea",
+//                 xValueFormatString: "DD-MM-YYY",
+//                 yValueFormatString: "$#,###.##",
+//                 dataPoints: dataPoints1
+//             }, {
+//                 name: "Average",
+//                 type: "line",
+//                 yValueFormatString: "$#,###.##",
+//                 dataPoints: dataPoints2
+//             }]
+//         }],
+//         navigator: {
+//             data: [{
+//                 dataPoints: dataPoints2
+//             }],
+//             axisX: {
+//                 labelFontColor: "transparent",
+//                 labelFontWeight: "bolder",
+//             },
+//             slider: {
+//                 minimum: new Date(2020, 0o0),
+//                 maximum: new Date(2021, 12)
+//             }
+//         },
+//         rangeSelector: {
+//             buttons: [{
+//                 label: "1D",
+//                 range: 1,
+//                 rangeType: "day"
+//             }, {
+//                 label: "7D",
+//                 range: 7,
+//                 rangeType: "day"
+//             }, {
+//                 label: "1Y",
+//                 range: 1,
+//                 rangeType: "year"
+//             }, {
+//                 label: "All",
+//                 range: null,
+//                 rangeType: "all"
+//             }]
+//         }
+//     });
+//     console.log("Pushmethode runt. 2. Data bestaat uit:")
+//     console.log(dataMap)
+//     for (let date of Object.keys(dataMap)) {
+//         dataPoints1.push({x: new Date(date), y:[Number(dataMap[date].min), Number(dataMap[date].max)]});
+//         dataPoints2.push({x: new Date(date), y:Number(dataMap[date].avg)});
 //     }
 //     stockChart.render();
-// })
-
-}
+// }
